a094430b000531ea7550da1e71a352e28c0eb172
hizamakura <kaworinakano@gmail.com>
2020-02-02 00:15:42 +0800

    Do not include middle in search nodes

--- Scrabble/Models/Tile.cs
+++ Scrabble/Models/Tile.cs
@@ -40,52 +40,36 @@ namespace Scrabble.Models
 
         public void Search() { }
 
+        // This does not include the starting position, as we already know
+        // It is a guaranteed letter.
         public IEnumerable<Point> SearchGenerator(Direction direction)
         {
             switch (direction)
             {
                 case Direction.Up:
-                    for (int i = Origin.Position.Y; i >= 0; i--)
+                    for (int i = Origin.Position.Y - 1; i >= 0; i--)
                         yield return new Point(Origin.Position.X, i);
 
                     break;
                 case Direction.Down:
-                    for (int i = Origin.Position.Y; i < Board.Size; i++)
+                    for (int i = Origin.Position.Y + 1; i < Board.Size; i++)
                         yield return new Point(Origin.Position.X, i);
 
                     break;
                 case Direction.Left:
-                    for (int i = Origin.Position.X; i >= 0; i--)
+                    for (int i = Origin.Position.X - 1; i >= 0; i--)
                         yield return new Point(i, Origin.Position.Y);
 
                     break;
 
                 case Direction.Right:
-                    for (int i = Origin.Position.X; i < Board.Size; i++)
+                    for (int i = Origin.Position.X + 1; i < Board.Size; i++)
                         yield return new Point(i, Origin.Position.Y);
 
                     break;
                 case Direction.None: break;
                 default: throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
             }
-        }
-
-        public IEnumerable<Point> SearchGeneratorLinq(Direction direction)
-        {
-            return direction switch
-            {
-                Direction.Up => Enumerable.Range(0, Origin.Position.Y)
-                   .Reverse()
-                   .Select(y => new Point(Origin.Position.X, y)),
-                Direction.Down => Enumerable.Range(Origin.Position.Y, Board.Size)
-                   .Select(y => new Point(Origin.Position.X, y)),
-                Direction.Left => Enumerable.Range(0, Origin.Position.X)
-                   .Reverse()
-                   .Select(x => new Point(x, Origin.Position.Y)),
-                Direction.Right => Enumerable.Range(Origin.Position.X, Board.Size)
-                   .Select(x => new Point(x, Origin.Position.Y)),
-                _ => Enumerable.Empty<Point>()
-            };
         }
     }
 }
