adb1832f92d90dcaf0b1f8fab4bb8cc6642519f9
hizamakura <kaworinakano@gmail.com>
2020-02-04 08:05:07 +0800

    Qualify SearchNode into its own class

---
+++ Scrabble/Models/SearchNode.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Scrabble.Models
+{
+    public class SearchNode
+    {
+        public SearchNode(Tile origin, Board board, Orientation searchOrientation)
+        {
+            Origin = origin;
+            Board = board;
+            SearchOrientation = searchOrientation;
+        }
+
+        public SearchNode InverseNode(Point start)
+        {
+            return new SearchNode(Board.Tiles[start.Y][start.X], Board, Inverse(SearchOrientation));
+        }
+
+        public enum Direction
+        {
+            None,
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        public enum Orientation
+        {
+            None,
+            Vertical,
+            Horizontal
+        }
+
+        public static List<SearchNode> Nodes { get; } = new List<SearchNode>();
+
+        public Tile Origin { get; set; }
+
+        public Board Board { get; set; }
+
+        public Orientation SearchOrientation { get; set; }
+
+        public Point Start { get; set; }
+
+        public Point End { get; set; }
+
+        public void Search()
+        {
+            if (SearchOrientation == Orientation.Horizontal)
+            {
+                Start = Seek(Direction.Up);
+                End = Seek(Direction.Down);
+            }
+            else
+            {
+                Start = Seek(Direction.Left);
+                End = Seek(Direction.Right);
+            }
+        }
+
+        public Point Seek(Direction direction)
+        {
+            var result = Origin.Position;
+            foreach (var position in SearchGenerator(direction))
+            {
+                if(!Nodes.Any(n => n.Origin.Position == position && n.SearchOrientation == Inverse(SearchOrientation)))
+                    Nodes.Add(InverseNode(position));
+
+                if (position.IsEmpty)
+                    break;
+
+                result = position;
+            }
+            return result;
+        }
+
+        public Orientation Inverse(Orientation orientation) =>
+            orientation == Orientation.Horizontal ? Orientation.Vertical : Orientation.Horizontal;
+
+        // This does not include the starting position, as we already know
+        // It is a guaranteed letter.
+        public IEnumerable<Point> SearchGenerator(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    for (int i = Origin.Position.Y - 1; i >= 0; i--)
+                        yield return new Point(Origin.Position.X, i);
+
+                    break;
+                case Direction.Down:
+                    for (int i = Origin.Position.Y + 1; i < Board.Size; i++)
+                        yield return new Point(Origin.Position.X, i);
+
+                    break;
+                case Direction.Left:
+                    for (int i = Origin.Position.X - 1; i >= 0; i--)
+                        yield return new Point(i, Origin.Position.Y);
+
+                    break;
+
+                case Direction.Right:
+                    for (int i = Origin.Position.X + 1; i < Board.Size; i++)
+                        yield return new Point(i, Origin.Position.Y);
+
+                    break;
+                case Direction.None: break;
+                default:             throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+        }
+    }
+}
