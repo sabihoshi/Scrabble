fd0a3b777e6ae0fa9eba325a4483cdf70c54b69a
loliprotector <kaworinakano@gmail.com>
2020-03-13 02:56:41 +0800

    Simplify tile placement code
    * Improve naming

--- Scrabble/ViewModels/GameWindowViewModel.cs
+++ Scrabble/ViewModels/GameWindowViewModel.cs
@@ -1,18 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Scrabble.Events;
 using Scrabble.Models;
 using Scrabble.Models.Tile;
 using Stylet;
-using IContainer = StyletIoC.IContainer;
+using StyletIoC;
 
 namespace Scrabble.ViewModels
 {
     public class GameWindowViewModel : Screen, 
-        IHandle<TilePressedEvent<BoardTile>>, 
+        IHandle<TilePressedEvent<BoardTile>>,
         IHandle<TilePressedEvent<RackTile>>
     {
         private ITile? _selectedBoardTile;
@@ -34,25 +32,20 @@ namespace Scrabble.ViewModels
 
         public BoardViewModel Board { get; set; }
 
-        public List<(BoardTile boardTile, RackTile rackTile)> PlacedTiles { get; set; } = new List<(BoardTile, RackTile)>();
+        public List<(BoardTile boardTile, RackTile rackTile)> PlacedTiles { get; set; } =
+            new List<(BoardTile, RackTile)>();
 
         public List<Player> Players { get; set; } = new List<Player>();
 
         public void Handle(TilePressedEvent<BoardTile> message)
         {
             SelectTile(ref _selectedBoardTile, message.TilePressed);
-
-            if (_selectedRackTile != null)
-                PlaceTile();
         }
 
         public void Handle(TilePressedEvent<RackTile> message)
         {
             SelectTile(ref _selectedRackTile, message.TilePressed);
-
-            if (_selectedBoardTile != null)
-                PlaceTile();
-        } 
+        }
 
         public void DeselectTile(ref ITile? tile)
         {
@@ -63,7 +56,6 @@ namespace Scrabble.ViewModels
 
         public void SelectTile(ref ITile? target, ITile selected)
         {
-            
             if (target == selected)
             {
                 DeselectTile(ref target);
@@ -72,17 +64,17 @@ namespace Scrabble.ViewModels
 
             if (target != null)
                 target.IsHighlighted = false;
-            
+
             target = selected;
             target.IsHighlighted = true;
+
+            if (_selectedRackTile != null && _selectedBoardTile != null)
+                TilePlacement();
         }
 
         public void ConfirmMove()
         {
-            foreach (var tile in PlacedTiles)
-            {
-
-            }
+            foreach (var tile in PlacedTiles) { }
         }
 
         public void CancelMove()
@@ -96,17 +88,16 @@ namespace Scrabble.ViewModels
             PlacedTiles.Clear();
         }
 
-        public void PlaceTile()
+        public void TilePlacement()
         {
-            if (_selectedRackTile is null || _selectedBoardTile is null)
-                return;
-
             var boardTile = (BoardTile)_selectedBoardTile;
             var rackTile = (RackTile)_selectedRackTile;
 
             // Check if there is a tile placed already
-            if (boardTile.HasLetter) { SwapTile(rackTile, boardTile); }
-            else { PlayerPlacedTile(boardTile, rackTile); }
+            if (boardTile.HasLetter)
+                SwapTile(rackTile, boardTile);
+            else
+                PlayerTilePlacement(boardTile, rackTile);
 
             PlacedTiles.Add((boardTile, rackTile));
 
@@ -114,7 +105,7 @@ namespace Scrabble.ViewModels
             DeselectTile(ref _selectedRackTile);
         }
 
-        private void PlayerPlacedTile(BoardTile boardTile, RackTile rackTile)
+        private void PlayerTilePlacement(BoardTile boardTile, RackTile rackTile)
         {
             boardTile.Letter = rackTile.Letter;
             boardTile.PlacedBy = CurrentPlayer;
@@ -128,7 +119,7 @@ namespace Scrabble.ViewModels
             rackTile.Letter = boardTile.Letter;
             boardTile.Letter = temp;
 
-            var original = 
+            var original =
                 PlacedTiles.Single(x => x.boardTile == boardTile);
             PlacedTiles.Remove(original);
         }
